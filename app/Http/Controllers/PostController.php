ed47f388378d5984478a1692107f24be0766fdaf
Aashish Neupane <120081944+neupane32@users.noreply.github.com>
2024-08-31 09:33:52 +0545

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+
+        Post::create($request->all());
+
+        return redirect()->route('posts.index')
+            ->with('success', 'Post created successfully.');
+    }
+
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+        return view('posts.show', compact('post'));
+    }
+
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        return view('posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+
+        $post = Post::findOrFail($id);
+        $post->update($request->all()); 
+
+        return redirect()->route('posts.index')
+            ->with('success', 'Post updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        return redirect()->route('posts.index')
+            ->with('success', 'Post deleted successfully.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,42 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\URL;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Force HTTPS scheme (if necessary)
+URL::forceScheme('https');
+
+// Define the routes for the CRUD operations on posts
+
+// Display all posts (Home page)
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+// Show the form for creating a new post
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+
+// Store a new post in the database
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+// Display a specific post
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+// Show the form for editing a specific post
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+
+// Update a specific post in the database
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+
+// Delete a specific post from the database
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
